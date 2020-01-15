cd1bb26932580440357d10ab6ae0941ff4a072d3
batval <karampish@gmail.com>
2020-01-15 15:40:36 +0300

    Add jdbc(part two) - it's work!

--- src/main/java/com/batval/dao/UserDAO.java
+++ src/main/java/com/batval/dao/UserDAO.java
@@ -4,6 +4,7 @@ import com.batval.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -18,50 +19,43 @@ public class UserDAO {
     private static Connection connection;
 
     static {
-        String url  = null;
+        String url = null;
         String username = null;
         String password = null;
 
         //load db properties
-            try (InputStream in = UserDAO.class.getClassLoader().getResourceAsStream("persistence.properties")){
-                Properties properties = new Properties();
-                properties.load(in);
+        try (InputStream in = UserDAO.class.getClassLoader().getResourceAsStream("persistence.properties")) {
+            Properties properties = new Properties();
+            properties.load(in);
+            Class.forName(properties.getProperty("driverClassName"));
+            url = properties.getProperty("url");
+            username = properties.getProperty("username");
+            password = properties.getProperty("password");
 
-//                url = properties.getProperty(url);
-  //              username=properties.getProperty(username);
-    //            password=properties.getProperty(password);
-
-                url ="jdbc:mysql://localhost:3306/doit?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-                username = "root";
-                password = "admin";
-
-            }
-         catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
 
-            try {
-                connection =DriverManager.getConnection(url,username,password);
-                System.out.println("Connection successful!");
-            }
-            catch (SQLException ex)
-            {
-                System.out.println("Connection failed!");
-                ex.printStackTrace();
-            }
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection successful!");
+        } catch (SQLException ex) {
+            System.out.println("Connection failed!");
+            ex.printStackTrace();
+        }
     }
 
     public List<User> getAll() throws SQLException {
-        List<User> users  = new ArrayList<>();
+        List<User> users = new ArrayList<>();
         PreparedStatement ps = connection.prepareStatement("select * from users");
         ResultSet rs = ps.executeQuery();
-    while (rs.next()){
-        User user = new User();
-         user.setName(rs.getString(1));
-         user.setSurname(rs.getString(2));
-         user.setEmail(rs.getString(3));
-         users.add(user);
-    }
-    return users;
+        while (rs.next()) {
+            User user = new User();
+            user.setName(rs.getString(1));
+            user.setSurname(rs.getString(2));
+            user.setEmail(rs.getString(3));
+            users.add(user);
+        }
+        return users;
     }
 }
